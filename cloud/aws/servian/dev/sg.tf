a937e9faea4f511aa8ecea9af0724808c1001667
Rupasinghe2012 <castilahadies@gmail.com>
2022-07-07 16:54:40 +0530

    Configured postgres rds

--- cloud/aws/servian/dev/sg.tf
+++ cloud/aws/servian/dev/sg.tf
@@ -27,3 +27,42 @@ module "bastion_sg" {
 
   tags = var.tags
 }
+
+module "postgre_rds_sg" {
+  source = "../../../../modules/aws-sg"
+
+  vpc_id           = module.vpc.vpc_id
+  application_name = var.app_name
+  environment      = var.environment
+  role             = "db"
+
+  rules_with_cidr_as_source = [
+    {
+      rule_type : "egress",
+      from_port : "0",
+      to_port : "65535",
+      protocol : "-1",
+      source : ["0.0.0.0/0"]
+      description : "Allow all outbound traffic"
+    },
+    {
+      rule_type : "ingress",
+      from_port : var.postgres_rds_vars["port"],
+      to_port : var.postgres_rds_vars["port"],
+      protocol : "tcp",
+      source : ["${chomp(data.http.myip.body)}/32"]
+      description : "Default DB Port"
+    },
+  ]
+  rules_with_security_group_as_source = [
+    {
+      rule_type : "ingress",
+      from_port : var.postgres_rds_vars["port"],
+      to_port : var.postgres_rds_vars["port"],
+      protocol : "tcp",
+      source : module.bastion_sg.sg_id
+      description : "Bastion DB Access"
+    },
+  ]
+  tags = var.tags
+}
