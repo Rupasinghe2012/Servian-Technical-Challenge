16d94bb0553a7dab8745149ad1017b80707bd8ec
Rupasinghe2012 <castilahadies@gmail.com>
2022-07-07 10:55:32 +0530

    bucket name ouput added

--- modules/aws-s3-state/outputs.tf
+++ modules/aws-s3-state/outputs.tf
@@ -2,6 +2,10 @@ output "s3-bucket" {
   value = aws_s3_bucket.tf_state
 }
 
+output "s3-bucket-name" {
+  value = aws_s3_bucket.tf_state.bucket
+}
+
 output "dynamo-table" {
   value = aws_dynamodb_table.dynamodb_state_locking
 }
