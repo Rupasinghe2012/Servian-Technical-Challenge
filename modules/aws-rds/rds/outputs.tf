733adef664cb3f7ad176e23edbfc0b12fde1b3c3
Rupasinghe2012 <castilahadies@gmail.com>
2022-07-11 03:23:10 +0530

    updated aws-rds

--- modules/aws-rds/rds/outputs.tf
+++ modules/aws-rds/rds/outputs.tf
@@ -2,3 +2,18 @@ output "rds_instance_endpoint" {
   description = "RDS Endpoint"
   value       = aws_db_instance.rds.endpoint
 }
+
+output "rds_instance_address" {
+  description = "RDS Endpoint"
+  value       = aws_db_instance.rds.address
+}
+
+output "rds_username" {
+  description = "The master username for the database."
+  value       = aws_db_instance.rds.username
+}
+
+output "rds_password" {
+  description = "The master password for the database."
+  value       = module.db_master_pass.secret_value
+}
