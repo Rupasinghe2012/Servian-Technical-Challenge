98242c994b5b6a36c67613cab517f2d6a4f5bd27
Rupasinghe2012 <castilahadies@gmail.com>
2022-07-11 03:23:27 +0530

    updated aws-keypair module

--- modules/aws-keypair/outputs.tf
+++ modules/aws-keypair/outputs.tf
@@ -2,3 +2,7 @@ output "key_name" {
   description = "The name of the keypair"
   value       = aws_key_pair.key_pair.key_name
 }
+
+output "private_key" {
+  value = module.private_key_pem.secret_value
+}
